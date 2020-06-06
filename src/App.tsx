ca2e991454a4f9ca3ffb5344e114257678a471a4
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2020-06-06 20:24:48 +0200

    Add Search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import {FiSearch} from "react-icons/all";
 
 const App = () => {
-  return (
-    <div className="App">
-      <h1>PHOTO SEARCH FROM GIPHY</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>PHOTO SEARCH FROM GIPHY</h1>
+            <div>
+                <input type="text" placeholder="Key Words"/>
+                <button><FiSearch className="icon"/></button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
