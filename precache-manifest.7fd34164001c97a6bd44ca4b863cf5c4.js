64f9a8bec5ff167565cd73236596b6839d242452
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2020-06-07 11:36:07 +0200

    Updates

---
+++ precache-manifest.7fd34164001c97a6bd44ca4b863cf5c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc0961da4be498d091677300200baba",
+    "url": "/photo-search-from-ghipy/index.html"
+  },
+  {
+    "revision": "b300df476a4d164ef6ff",
+    "url": "/photo-search-from-ghipy/static/css/main.cde53645.chunk.css"
+  },
+  {
+    "revision": "15cb3c23be66fbc2959e",
+    "url": "/photo-search-from-ghipy/static/js/2.2558b958.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/photo-search-from-ghipy/static/js/2.2558b958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b300df476a4d164ef6ff",
+    "url": "/photo-search-from-ghipy/static/js/main.af55bef9.chunk.js"
+  },
+  {
+    "revision": "74528d0cb3370b250b87",
+    "url": "/photo-search-from-ghipy/static/js/runtime-main.a0bef49c.js"
+  }
+]);
